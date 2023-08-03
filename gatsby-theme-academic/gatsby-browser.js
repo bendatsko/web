04456f1e765452e8354fd2a9c4df6ce86c3cd616
bdatsko <bdatsko@umich.edu>
2023-08-03 13:55:31 -0400

    Added support for Vercel analytics

--- gatsby-theme-academic/gatsby-browser.js
+++ gatsby-theme-academic/gatsby-browser.js
@@ -4,8 +4,3 @@ import {
 
 export const wrapPageElement = _wrapPageElement;
 
-export const onClientEntry = async () => {
-  if (typeof IntersectionObserver === "undefined") {
-    await import("intersection-observer")
-  }
-}
